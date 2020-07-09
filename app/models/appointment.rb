2a0e29cad86b2a08708bcbea2dce7ed14294dc45
MMcClure11 <mcclme02@luther.edu>
2020-07-09 10:24:04 -0500

    creates show page for appointment and writes method for formatting date

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,11 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  def format_date
+    s = self.appointment_datetime.to_datetime.to_formatted_s(:long_ordinal).split
+    s.insert(3, "at").join(" ").gsub("th", "")
+  end
 end
+
+
