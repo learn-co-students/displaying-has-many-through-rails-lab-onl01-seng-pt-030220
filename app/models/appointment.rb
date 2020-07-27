4ef00197e459442896c831e8813f311281520b0f
cdominick091984 <cdominick091984@gmail.com>
2020-07-26 21:35:58 -0400

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,11 +6,28 @@ class Appointment < ApplicationRecord
         self.appointment_datetime.strftime("%B %d, %Y at %T")
     end
 
-    def patient_name
-        self.patient.name
-    end
+    # def patient_name
+    #     self.patient.name
+    # end
 
-    def doctor_name
-        self.doctor.name
-    end
+    # def doctor_name
+    #     self.doctor.name
+    # end
+
+    # lab solution:
+    def doctor_name=(name)
+        self.doctor = Doctor.find_or_create_by(name: name)
+      end
+   
+      def doctor_name
+         self.doctor ? self.doctor.name : nil
+      end
+   
+     def patient_name=(name)
+       self.patient = Patient.find_or_create_by(name: name)
+     end
+   
+     def patient_name
+       self.patient ? self.patient.name : nil
+     end
 end
