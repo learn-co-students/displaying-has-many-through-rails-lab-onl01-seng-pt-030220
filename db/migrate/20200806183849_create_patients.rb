1d1328694c03494aa456f9110f04b67915c79e56
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-06 15:07:11 -0400

    Done.

--- db/migrate/20200806183849_create_patients.rb
+++ db/migrate/20200806183849_create_patients.rb
@@ -1,10 +1,10 @@
 class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "age"
+      t.string   "name"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.integer  "age"
     end
   end
 end
